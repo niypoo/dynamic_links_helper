3cea4d23197ea68f4050eebc6c36eede6b50b304
Mahmoud Nabhan <niypoo@gmail.com>
2024-12-16 12:55:38 +0200

    deprecated firebase dynamic link and use applinks

--- lib/abstracts/dynamicLinkHandler.abstract.dart
+++ lib/abstracts/dynamicLinkHandler.abstract.dart
@@ -2,7 +2,7 @@ import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 
 abstract class DynamicLinkServiceHandler {
   // abstract
-  void handler(PendingDynamicLinkData? dynamicLink);
+  void handler(Uri? dynamicLink);
 
   // Get Path
   String? getPath(PendingDynamicLinkData dynamicLink) => dynamicLink.link.path;
--- lib/dynamicLinks.helper.dart
+++ lib/dynamicLinks.helper.dart
@@ -1,63 +1,31 @@
 import 'package:dynamic_links_helper/abstracts/dynamicLinkHandler.abstract.dart';
-import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:app_links/app_links.dart';
 
 class DynamicLinksHelper {
+  static final _appLinks = AppLinks();
+
   // Initialization
   static Future<void> init({
     required DynamicLinkServiceHandler handler,
   }) async {
-    // Try to get initial value (app just open)
-    handler.handler(await FirebaseDynamicLinks.instance.getInitialLink());
     // Listen to dynamic (app has opened)
-    FirebaseDynamicLinks.instance.onLink.listen(
-        (PendingDynamicLinkData dynamicLink) => handler.handler(dynamicLink));
+    _appLinks.uriLinkStream.listen(handler.handler)
+    .onError((error) {
+      print('Dynamic Link Handler $error');
+    });
+
+    // Try to get initial value (app just open)
+    handler.handler(await _appLinks.getInitialLink());
   }
 
   // create link
   static Future<Uri> create({
-    required String uriPrefix,
     required String domain,
-    required String bundleId,
-    required String appStoreIdentifier,
-    required String socialImage,
-    required String path,
-    required String socialTitle,
-    required String socialDescription,
     Map<String, dynamic>? queryParameters,
-    bool shortLink = true,
   }) async {
+
     // define url as payload
-    Uri url;
-
-    // initialize a dynamic link
-    final DynamicLinkParameters parameters = DynamicLinkParameters(
-      uriPrefix: uriPrefix,
-      link: Uri.https(domain, path, queryParameters),
-      androidParameters: AndroidParameters(
-        packageName: bundleId,
-        minimumVersion: 0,
-      ),
-      iosParameters: IOSParameters(
-        bundleId: bundleId,
-        appStoreId: appStoreIdentifier,
-        minimumVersion: '0',
-      ),
-      socialMetaTagParameters: SocialMetaTagParameters(
-        title: socialTitle,
-        description: socialDescription,
-        imageUrl: Uri.parse(socialImage),
-      ),
-    );
-
-    // build link as short or normal
-    if (shortLink) {
-      final ShortDynamicLink shortenedLink =
-          await FirebaseDynamicLinks.instance.buildShortLink(parameters);
-
-      url = shortenedLink.shortUrl;
-    } else {
-      url = await FirebaseDynamicLinks.instance.buildLink(parameters);
-    }
+    Uri url = Uri(scheme: domain ,queryParameters:queryParameters);
 
     // return payload
     return url;
