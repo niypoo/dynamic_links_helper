4a1a75f15b64d439aa01a6dca18d7e322c2dd239
Mahmoud Nabhan <niypoo@gmail.com>
2024-12-18 14:38:22 +0200

    Update dynamicLinkHandler.abstract.dart

--- lib/abstracts/dynamicLinkHandler.abstract.dart
+++ lib/abstracts/dynamicLinkHandler.abstract.dart
@@ -3,9 +3,18 @@ abstract class DynamicLinkServiceHandler {
   void handler(Map<dynamic, dynamic>? dynamicLink);
 
   // Get Path
-  String? getPath(Map<dynamic, dynamic> dynamicLink) => dynamicLink['path'];
+  String? getPath(Map<dynamic, dynamic>? dynamicLink) {
+    // skip
+    if (dynamicLink == null || !dynamicLink.containsKey('path')) return null;
+    // return payload
+    return dynamicLink['path'];
+  }
 
   // Get Params
-  Map<String, String> getParams(Map<dynamic, dynamic> dynamicLink) =>
-      Map.from((dynamicLink['queryParameters'] ?? ''));
+  Map<String, String>? getParams(Map<dynamic, dynamic>? dynamicLink) {
+    // skip
+    if (dynamicLink == null || !dynamicLink.containsKey('queryParameters')) return null;
+    // return payload
+    return Map.from((dynamicLink['queryParameters']));
+  }
 }
